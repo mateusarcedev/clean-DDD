a09c49949768f16c5942c15e93eaea4f05900b05
Mateus Arce <arcemateuss@gmail.com>
2025-07-12 18:45:21 -0400

    Caso de uso: Listar perguntas recentes

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec..ts
@@ -0,0 +1,61 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answers-repository'
+import { DeleteAnswerUseCase } from './delete-answer'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswerRepository
+let sut: ChooseQuestionBestAnswerUseCase
+
+describe('Choose question best A nswer', async () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswerRepository()
+    sut = new ChooseQuestionBestAnswerUseCase(inMemoryQuestionsRepository, inMemoryAnswersRepository)
+  })
+
+  it('should be able to choose question best answer', async () => {
+    const question = makeQuestion()
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      authorId: answer.id.toString(),
+      answerId: question.authorId.toString()
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(answer.id)
+  })
+
+  it('should not be able to choose another user best answer', async () => {
+    const question = makeQuestion({
+      authorId: new UniqueEntityID('author-1')
+    })
+
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    await expect(() => {
+      return sut.execute({
+        authorId: answer.id.toString(),
+        answerId: 'author-1'
+      })
+    }).rejects.toBeInstanceOf(Error)
+
+  })
+
+
+})
---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -0,0 +1,47 @@
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { FetchRecentQuestionsUseCase } from './fetch-recent-questions'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: FetchRecentQuestionsUseCase
+
+describe('Fetch Recent Questions', async () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to fetch recent questions', async () => {
+    await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date('2025-07-10T00:00:00.000Z') }))
+    await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date('2025-07-11T00:00:00.000Z') }))
+    await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date('2025-07-12T00:00:00.000Z') }))
+
+
+    const { questions } = await sut.execute({
+      page: 1,
+    })
+
+    expect(questions.map(q => q.createdAt.toISOString())).toEqual([
+      new Date('2025-07-12').toISOString(),
+      new Date('2025-07-11').toISOString(),
+      new Date('2025-07-10').toISOString()
+    ])
+  })
+
+  it('should be able to fetch paginated recent questions', async () => {
+
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionsRepository.create(makeQuestion())
+    }
+
+    const { questions } = await sut.execute({
+      page: 2,
+    })
+
+    expect(questions).toHaveLength(2)
+  })
+
+
+})
